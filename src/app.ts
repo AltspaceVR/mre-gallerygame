fb17f7f22b1a8ffe7955acf940d3c1dfd5545b2f
Andrew Hinojosa <v-anhino@microsoft.com>
2019-07-03 16:18:46 -0700

    Created balloon pop collision with the dart and sphere

--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,12 @@ import {
     Actor,
     AnimationEaseCurves,
     ButtonBehavior,
-    Collider,
-    CollisionDetectionMode,
-    Color3,
     Context,
-    ForwardPromise,
     PrimitiveShape,
     Quaternion,
-    RigidBody,
-    TargetBehavior,
     TextAnchorLocation,
     User,
-    Vector2,
-    Vector3,
-    Vector4,
 } from '@microsoft/mixed-reality-extension-sdk';
-import { isPrimitive } from 'util';
-import { DESTRUCTION } from 'dns';
 /**
  * The main class of this app. All the logic goes here.
  */
@@ -30,20 +19,14 @@ export default class GameGallery {
     private root: Actor;
     private sphere: Actor;
     private dart: Actor;
-    private dartPromise: Actor;
-    private spherePromise: Actor;
-    private textPromise: Actor;
-    private playerOne: Actor;
-
     private sphereArray: Actor[] = [];
 
     constructor(private context: Context, private baseUrl: string) {
         this.context.onStarted(() => this.started());
         this.context.onUserJoined((user) => this.userJoined(user));
     }
-
     private userJoined(user: User) {
-     const playerOnePromise = Actor.CreateEmpty(this.context, {
+        const playerOnePromise = Actor.CreateEmpty(this.context, {
             actor: {
                 attachment: {
                     userId: user.id,
@@ -53,30 +36,19 @@ export default class GameGallery {
                 subscriptions: ['transform']
             },
         });
-     this.playerOne = playerOnePromise.value;
     }
     /**
      * Once the context is "started", initialize the app.
      */
     private started() {
-        const textPromise = Actor.CreateEmpty(this.context, {
-            actor: {
-                name: 'Text',
-                transform: {
-                    app: { position: { x: 0, y: 0.5, z: 0 } }
-                },
-                text: {
-                    contents: "Gallery Game",
-                    anchor: TextAnchorLocation.TopLeft,
-                    color: { r: 30 / 255, g: 206 / 255, b: 213 / 255 },
-                    height: 0.3
-                }
-            }
-        });
         this.createRootActor();
         this.launchSphere();
         this.createDart();
         this.sphereBehavior();
+        this.dart.collider.onTrigger('trigger-exit', () => {
+            console.log("do something");
+            this.cancelSphere();
+        });
     }
     private cancelSphere() {
         this.sphere.destroy();
@@ -113,11 +85,17 @@ export default class GameGallery {
                                 scale: { x: 0.3, y: 0.4, z: 0.3 },
                             }
                         },
-                        // collider: {
-                        //     isTrigger: true
-                        // }
                     }
                 });
+                spherePromise.then(actor => {
+                    actor.collider.isTrigger = true;
+                    actor.collider.onTrigger('trigger-enter', (otherActor: Actor) => {
+                        console.log("other actor name: " + otherActor.name);
+                        if (otherActor.parent.name === "throwing_dart") {
+                            actor.destroy();
+                        }
+                    });
+                }).catch();
                 this.sphereArray.push(spherePromise.value);
             }
         }
@@ -127,7 +105,7 @@ export default class GameGallery {
             // at the given URL
             resourceUrl: `${this.baseUrl}/11750_throwing_dart_v1_L3.glb`,
             // and spawn box colliders around the meshes.
-            colliderType: 'mesh',
+            colliderType: 'sphere',
             // Also apply the following generic actor properties.
             actor: {
                 parentId: this.root.id,
@@ -139,21 +117,20 @@ export default class GameGallery {
                         rotation: Quaternion.FromEulerAngles(0, -Math.PI, 0),
                     }
                 },
-                grabbable: true,
+                rigidBody: {
+                    isKinematic: true,
+                },
+                grabbable: true
             }
         });
         this.dart = dartPromise.value;
         this.dart.enableRigidBody({ detectCollisions: true, useGravity: false });
-        // this.dart.collider.isTrigger = true;
-        this.dart.collider.onCollision('collision-enter', ()=>{
-            console.log("heeey boo")
-            this.cancelSphere();
+        this.dart.collider.onCollision('collision-enter', () => {
+            console.log('heeeyyyyy')
         })
-
-
-        this.dart.onGrab("end", (user) => {
-// tslint:disable-next-line: max-line-length
-
+        // this.dart.collider.isTrigger = true;
+        this.dart.onGrab("end", () => {
+            // tslint:disable-next-line: max-line-length
             this.dart.animateTo({ transform: { app: { position: { z: this.sphere.transform.app.position.z + 100 } } } }, 5, AnimationEaseCurves.EaseOutSine);
             this.dart.rigidBody.velocity = 0;
         });
